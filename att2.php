2d7902ad9e3f98a60f3b186c2ce6352246717076
SarahElizabeteTDS <sarah.elizabete.tds2023@gmail.com>
2024-07-24 01:20:00 -0300

    att2

---
+++ att2.php
@@ -0,0 +1,30 @@
+<?php
+
+class Retangulo{
+
+    //criando os atributos
+    public $base;
+    public $altura;
+
+    //criando os metodos
+    function area(){
+        $area = $this->base * $this->altura;
+        return $area;
+    }
+
+    function perimetro(){
+        $perimetro = $this->base * 2 + $this->altura * 2;
+        return $perimetro;
+    }
+}
+
+for ($i=0; $i < 3; $i++) { 
+    
+    $retangulo = new Retangulo();
+
+    $retangulo -> base = readline("Insira a base do retângulo " . $i+1 . ": ");
+    $retangulo -> altura = readline("Insira a altura do retângulo " . $i+1 . ": ");
+    
+    print "O valor da área é de: " . $retangulo->area() . "\n";
+    print "O valor do perimetro é de: " . $retangulo->perimetro() . "\n";
+}
